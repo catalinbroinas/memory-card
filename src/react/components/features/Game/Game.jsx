eb8dc0eb393a181bd514bf131b4dd6db563f80ec
catalinbroinas <catalinbroinas@yahoo.com>
2026-04-30 13:25:46 +0300

    feat(StartScreen): add StartScreen component

--- src/react/components/features/Game/Game.jsx
+++ src/react/components/features/Game/Game.jsx
@@ -1,8 +1,11 @@
+import StartScreen from "./StartScreen/StartScreen";
 
 function Game() {
   return (
     <section className="game">
       <h1 className="game__title">Memory Card</h1>
+
+      <StartScreen />
     </section>
   );
 }
---
+++ src/react/components/features/Game/StartScreen/StartScreen.jsx
@@ -0,0 +1,8 @@
+
+function StartScreen() {
+  return (
+    <section className="start-screen"></section>
+  );
+}
+
+export default StartScreen;
