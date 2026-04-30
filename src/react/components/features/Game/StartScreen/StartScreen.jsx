9b783a7f8bee81da1c3f7de8544d29ec423f4885
catalinbroinas <catalinbroinas@yahoo.com>
2026-04-30 19:04:46 +0300

    feat(StartScreen): improve accessibility

--- src/react/components/features/Game/StartScreen/StartScreen.jsx
+++ src/react/components/features/Game/StartScreen/StartScreen.jsx
@@ -4,6 +4,8 @@ import GameDifficulty from "./GameDifficulty";
 function StartScreen() {
   return (
     <section className="start-screen">
+      <h2 className="visually-hidden">Start screen</h2>
+
       <GameInfo />
       <GameDifficulty />
     </section>
