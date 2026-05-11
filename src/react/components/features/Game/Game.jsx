396a6849b34451aca2397277b853e747ecd58900
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-11 13:05:35 +0300

    feat(constants): add game flow constants

--- src/react/components/features/Game/Game.jsx
+++ src/react/components/features/Game/Game.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
+// Date
+import { GAME_STATUS } from "../../../constants/gameConstants";
+
+// Components
 import StartScreen from "./StartScreen/StartScreen";
 
 function Game() {
-  const [gameStatus, setGameStatus] = useState('start');
+  const [gameStatus, setGameStatus] = useState(GAME_STATUS.START);
 
   return (
     <section className="game">
       <h1 className="game__title">Memory Card</h1>
 
-      {gameStatus === 'start' && (
+      {gameStatus === GAME_STATUS.START && (
         <StartScreen />
       )}
     </section>
---
+++ src/react/constants/gameConstants.js
@@ -0,0 +1,14 @@
+
+// Enums
+export const DIFFICULTY = {
+  EASY: 'easy',
+  MEDIUM: 'medium',
+  HARD: 'hard'
+};
+
+// States
+export const GAME_STATUS = {
+  START: 'start',
+  PLAYING: 'playing',
+  END: 'end'
+};
