555e6ce0260c598c5ced5ab9a8846e55890d354e
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-13 17:52:29 +0300

    refactor(GameScreen): remove temporary UI

--- src/react/components/features/Game/GameScreen/GameScreen.jsx
+++ src/react/components/features/Game/GameScreen/GameScreen.jsx
@@ -1,11 +1,8 @@
-import CardGrid from "./CardGrid";
 
 function GameScreen() {
   return (
     <section className="game-screen">
       <h2 className="visually-hidden">Play game</h2>
-
-      <CardGrid />
     </section>
   );
 }
