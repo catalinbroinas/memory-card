71096a2527fdca076f518e0fbb472d1c502f79bf
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-14 17:44:55 +0300

    feat(cards): add initial card dataset

---
+++ src/react/data/cards/cardImages.js
@@ -0,0 +1,15 @@
+import londonImg from "../../../assets/images/london.webp";
+import parisImg from "../../../assets/images/paris.webp";
+import berlinImg from "../../../assets/images/berlin.webp";
+import madridImg from "../../../assets/images/madrid.webp";
+import romaImg from "../../../assets/images/roma.webp";
+import bucurestiImg from "../../../assets/images/bucuresti.webp";
+
+export const cardImages = {
+  london: londonImg,
+  paris: parisImg,
+  berlin: berlinImg,
+  madrid: madridImg,
+  roma: romaImg,
+  bucuresti: bucurestiImg
+};
---
+++ src/react/data/cards/cardList.js
@@ -0,0 +1,34 @@
+import { cardImages } from "./cardImages";
+
+export const cards = [
+  {
+    id: crypto.randomUUID(),
+    name: "London",
+    imageUrl: cardImages.london
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Paris",
+    imageUrl: cardImages.paris
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Berlin",
+    imageUrl: cardImages.berlin
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Madrid",
+    imageUrl: cardImages.madrid
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Roma",
+    imageUrl: cardImages.roma
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "București",
+    imageUrl: cardImages.bucuresti
+  }
+];
