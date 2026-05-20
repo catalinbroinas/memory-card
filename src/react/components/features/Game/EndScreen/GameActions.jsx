35c02966374c5f758b8c57130cf86c9c4c4379e4
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-20 11:10:15 +0300

    feat(GameActions): add action callbacks structure

--- src/react/components/features/Game/EndScreen/GameActions.jsx
+++ src/react/components/features/Game/EndScreen/GameActions.jsx
@@ -1,9 +1,16 @@
 
-function GameActions() {
+function GameActions({ onReset, onReplay }) {
   return (
     <div className="game-actions" role="group" aria-label="Game actions">
-      <button className="btn-primary-outline">Restart</button>
-      <button className="btn-primary">Play again</button>
+      <button
+        className="btn-primary-outline"
+        onClick={onReset}
+      >Restart</button>
+
+      <button
+        className="btn-primary"
+        onClick={onReplay}
+      >Play again</button>
     </div>
   );
 }
