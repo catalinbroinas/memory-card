526cef50bb81eb82525316c21d315d736cd1ca3a
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-22 09:53:15 +0300

    feat(cards): populate cards data

--- src/react/data/cards/cardImages.js
+++ src/react/data/cards/cardImages.js
@@ -4,6 +4,14 @@ import berlinImg from "../../../assets/images/berlin.webp";
 import madridImg from "../../../assets/images/madrid.webp";
 import romaImg from "../../../assets/images/roma.webp";
 import bucurestiImg from "../../../assets/images/bucuresti.webp";
+import dublinImg from "../../../assets/images/dublin.webp";
+import glasgowImg from "../../../assets/images/glasgow.webp";
+import lisbonImg from "../../../assets/images/lisbon.webp";
+import pragueImg from "../../../assets/images/prague.webp";
+import osloImg from "../../../assets/images/oslo.webp";
+import moscovaImg from "../../../assets/images/moscova.webp";
+import belfastImg from "../../../assets/images/belfast.webp";
+import amsterdamImg from "../../../assets/images/amsterdam.webp";
 
 export const cardImages = {
   london: londonImg,
@@ -11,5 +19,13 @@ export const cardImages = {
   berlin: berlinImg,
   madrid: madridImg,
   roma: romaImg,
-  bucuresti: bucurestiImg
+  bucuresti: bucurestiImg,
+  dublin: dublinImg,
+  glasgow: glasgowImg,
+  lisbon: lisbonImg,
+  prague: pragueImg,
+  oslo: osloImg,
+  moscova: moscovaImg,
+  belfast: belfastImg,
+  amsterdam: amsterdamImg
 };
--- src/react/data/cards/cardList.js
+++ src/react/data/cards/cardList.js
@@ -30,5 +30,45 @@ export const cards = [
     id: crypto.randomUUID(),
     name: "București",
     imageUrl: cardImages.bucuresti
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Lisbon",
+    imageUrl: cardImages.lisbon
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Glasgow",
+    imageUrl: cardImages.glasgow
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Dublin",
+    imageUrl: cardImages.dublin
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Belfast",
+    imageUrl: cardImages.belfast
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Moscova",
+    imageUrl: cardImages.moscova
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Prague",
+    imageUrl: cardImages.prague
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Oslo",
+    imageUrl: cardImages.oslo
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Amsterdam",
+    imageUrl: cardImages.amsterdam
   }
 ];
