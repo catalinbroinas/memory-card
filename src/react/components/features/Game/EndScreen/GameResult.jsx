b0727299dbbd06bcfbedb465008bbe53e65d1752
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-23 16:30:39 +0300

    feat(GameResult): add static UI for game result

--- src/react/components/features/Game/EndScreen/GameResult.jsx
+++ src/react/components/features/Game/EndScreen/GameResult.jsx
@@ -1,22 +1,26 @@
 
 function GameResult({ difficulty, score, bestScore }) {
   return (
-    <ul className="game-result">
-      <li className="game-result__item">
-        <span className="game-result__label">Difficulty:</span>
-        <strong className="game-result__value">{difficulty}</strong>
-      </li>
+    <div className="game-result">
+      <h3 className="game-result__title">Win / Lose</h3>
 
-      <li className="game-result__item">
-        <span className="game-result__label">Your score:</span>
-        <strong className="game-result__value">{score}</strong>
-      </li>
+      <ul className="game-result__list">
+        <li className="game-result__item">
+          <span className="game-result__label">Difficulty:</span>
+          <strong className="game-result__value">{difficulty}</strong>
+        </li>
 
-      <li className="game-result__item">
-        <span className="game-result__label">Best score:</span>
-        <strong className="game-result__value">{bestScore}</strong>
-      </li>
-    </ul>
+        <li className="game-result__item">
+          <span className="game-result__label">Your score:</span>
+          <strong className="game-result__value">{score}</strong>
+        </li>
+
+        <li className="game-result__item">
+          <span className="game-result__label">Best score:</span>
+          <strong className="game-result__value">{bestScore}</strong>
+        </li>
+      </ul>
+    </div>
   );
 }
 
