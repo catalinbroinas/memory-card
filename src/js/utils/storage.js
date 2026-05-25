445c710e7e559751fddf7730a146a76562826047
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-25 18:23:00 +0300

    feat(storage): add localStorage utilities

--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -9,3 +9,27 @@ export function isLocalStorageAvailable() {
     return false;
   }
 };
+
+export function getStorageItem(key) {
+  if (!isLocalStorageAvailable()) return;
+
+  return localStorage.getItem(key);
+}
+
+export function setStorageItem(key, value) {
+  if (!isLocalStorageAvailable()) return;
+
+  localStorage.setItem(key, value);
+}
+
+export function removeStorageItem(key) {
+  if (!isLocalStorageAvailable()) return;
+
+  localStorage.removeItem(key);
+}
+
+export function clearStorage() {
+  if (!isLocalStorageAvailable()) return;
+
+  localStorage.clear();
+}
