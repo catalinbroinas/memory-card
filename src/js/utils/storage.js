9bb1f4e43e9e47eb69f6cefb07d3df405809c88a
catalinbroinas <catalinbroinas@yahoo.com>
2026-05-27 20:17:02 +0300

    refactor(storage): validate localStorage only on request

--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -2,8 +2,10 @@
 export function isLocalStorageAvailable() {
   try {
     const key = "__test__";
+
     localStorage.setItem(key, key);
     localStorage.removeItem(key);
+
     return true;
   } catch {
     return false;
@@ -11,25 +13,17 @@ export function isLocalStorageAvailable() {
 };
 
 export function getStorageItem(key) {
-  if (!isLocalStorageAvailable()) return;
-
   return localStorage.getItem(key);
 }
 
 export function setStorageItem(key, value) {
-  if (!isLocalStorageAvailable()) return;
-
   localStorage.setItem(key, value);
 }
 
 export function removeStorageItem(key) {
-  if (!isLocalStorageAvailable()) return;
-
   localStorage.removeItem(key);
 }
 
 export function clearStorage() {
-  if (!isLocalStorageAvailable()) return;
-
   localStorage.clear();
 }
